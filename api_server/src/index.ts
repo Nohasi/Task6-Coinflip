559fa179212d2c5c58aa5416173bc2592b485190
Trainee (Mohammed Abdulla Mohammed Hassan Alhosani) <MAB52339@emiratesnbd.com>
2023-01-31 11:05:50 +0400

    Added error checking for parameters

--- api_server/src/index.ts
+++ api_server/src/index.ts
@@ -7,7 +7,21 @@ const app = express();
 app.use(express.json());
 
 app.get('/coinflip', (req: express.Request, res: express.Response) => {
-
+    let errorMessage: string | null = invalidRequest(req);
+    if(errorMessage != null){
+        res.status(406);
+        res.send({
+            status: 406,
+            error: errorMessage
+        })
+        return;
+    }
+    res.status(200);
+    res.send({
+        status: 200,
+        flips : req.query['flips'],
+        side: req.query['side']
+    });
 });
 
 const port: number = 4090;
